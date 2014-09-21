1a1ade91f6898a3b539dd01f648903ea069c4c83
Danil Korotenko <danil.korotenko@gmail.com>
2014-09-22 00:14:22 +0300

    Started to develop game.

---
+++ main.lua
@@ -0,0 +1,24 @@
+-- $Name: Alien$
+-- $Version:1.0.0$
+-- $Author:Danil Korotenko$
+
+game.dsc = [[Game by film story.]];
+
+instead_version '1.6.0';
+
+game.codepage = 'UTF-8';
+game.act = function(o, b)
+	return 'ERROR: "act" undefined for "'..tostring(b.nam)..'"';
+end;
+
+game.inv = 'ERROR: "inv" undefined';
+game.use = 'ERROR: "use" undefined';
+game.forcedsc = true;
+
+dofile('alien.lua');
+
+main = room {
+	nam = 'Alien.',
+	dsc = "(С) September 2014. Danil Korotenko (danil.korotenko@gmail.com)",
+	obj = { vway("Next", "Click {there} to play.", 'spaceShip') },
+};
