0a3da4b9fc1139ac6b530123e26d2a507e535d1a
Danil Korotenko <danil.korotenko@gmail.com>
2014-11-01 15:40:54 +0200

    Removed unneeded text from alien.lua.

--- alien.lua
+++ alien.lua
@@ -4599,85 +4599,7 @@ PASSAGEWAY__B_LEVEL = room
 MESS = room
 {
 	nam = "MESS",
-	dsc = [[----------------------------------------------------------------
-^
-^        The crew of the United States commercial starship Nostromo
-^        seated around a table.
-^
-^                 Dallas.........Captain
-^                 Kane...........Executive Officer
-^                 Ripley.........Warrant Officer
-^                 Ash............Science Officer
-^                 Lambert........Navigator
-^                 Parker.........Engineer
-^                 Brett..........Engineering Technician
-^                 Jones..........Cat
-^              
-^        Five men and two women: Lambert and Ripley.
-^
-^                                 LAMBERT
-^                  Jesus am I cold.
-^
-^                                 PARKER
-^                  Still with us, Brett.
-^
-^                                 BRETT
-^                  Yo.
-^
-^                                 RIPLEY
-^                  Lucky us.
-^
-^        They yawn, stretch, shiver.
-^        Dallas looks over at a flashing yellow light.
-^
-^                                 KANE
-^                  I feel dead.
-^
-^        Kane is not yet fully awake.
-^        Yawns.
-^
-^                                 PARKER
-^                  You look dead.
-^
-^                                 ASH
-^                  Nice to be back.
-^
-^                                 PARKER
-^                  Before we dock maybe we'd
-^                  better go over the bonus
-^                  situation.
-^
-^                                 BRETT
-^                  Yeah.
-^
-^                                 PARKER
-^                  Brett and I think we deserve a
-^                  full share.
-^
-^                                 DALLAS
-^                  You two will get what you
-^                  contracted for.  Just like
-^                  everybody else.
-^
-^                                 BRETT
-^                  Everybody else gets more than us.
-^   
-^                                 DALLAS
-^                  Everybody else deserves more
-^                  than you two.
-^
-^                                 ASH
-^                  Mother wants to talk to you.
-^
-^                                 DALLAS
-^                  I saw it.  Yellow light for my
-^                  eyes only...Now, everybody hit
-^                  their stations.
-^
-^********************************************************************
-^* End of Scene 2
-^********************************************************************
-^
+	dsc = [[
 ----------------------------------------------------------------
 ^
 ^        Entire crew present.
@@ -9688,113 +9610,6 @@ BRIDGE = room
 ^
 ----------------------------------------------------------------
 ^
-^        Above eye level the room is ringed by viewscreens.
-^        All of them blank.
-^        Kane, Ripley, and Lambert enter.
-^        Dallas' seat remains empty.
-^        All of them now dressed; they find their way to individual
-^        consoles.
-^        Ripley puts down the cat, straps herself into the high-
-^        backed chair.
-^
-^                                 KANE
-^                  Plug us in.
-^
-^        All three crew members begin throwing switches.
-^        The control room starts to come to life.
-^        Colored lights flicker.
-^        Chase each other across glowing screens.
-^
-^                                 KANE
-^                  Give us something to look at.
-^                 
-^        Lambert presses a bank of switches.
-^        Viewscreens glimmer into life.
-^
-^                                 LAMBERT
-^                  Take a look at this.
-^
-^        On each screen, blackness speckled with stars.
-^
-^                                 LAMBERT
-^                  Where's Earth.
-^              
-^                                 KANE
-^                  You're the navigator.
-^              
-^                                 RIPLEY
-^                  That's not our system.
-^
-^                                 KANE
-^                  Scan.
-^              
-^        Lambert hits several toggles.
-^        On the screens the images begin to drift.
-^   
-^        ONE OF THE SCREENS
-^
-^        A moving image of a starfield.
-^
-----------------------------------------------------------------
-^
-^        Lambert pores over charts.
-^        Consults her console.
-^        Puzzled.
-^
-^                                 KANE
-^                  Contact traffic control.
-^              
-^        Ripley switches on her transmission unit.
-^
-^                                 RIPLEY
-^                  This is commercial vessel Nostromo. 
-^                  Registration number 180246.  Do
-^                  you read me.  Over.
-^              
-^        Nothing but the hiss of static.
-^
-^                                 RIPLEY
-^                  Nothing.
-^              
-^                                 KANE
-^                  Keep trying.
-^
-^        Turns to Lambert.
-^        Ripley attempting transmission in b.g.
-^
-^                                 KANE
-^                  You got a reading yet.
-^              
-^                                 LAMBERT
-^                  We're way out in the boondocks
-^                  here...
-^              
-^                                 KANE
-^                  Keep trying...
-^              
-^                                 LAMBERT
-^                  Working on it.
-^
-^        Eureka.
-^
-^                                 LAMBERT
-^                  Found it.
-^
-^                                 KANE
-^                  Hard to believe.
-^
-^                                 LAMBERT
-^                  What the hell are we doing out
-^                  here.
-^
-^                                 KANE
-^                  What are you talking about.
-^              
-^                                 RIPLEY
-^                  It's not our system.
-^                                                           
-----------------------------------------------------------------
-^
 ^        Dallas at his console speaking to Ash.
 ^
 ^                                 DALLAS
@@ -10517,7 +10332,6 @@ BRIDGE = room
 'COMPUTER_ANNEX' , 
 'FOOD_LOCKER_NUMBER_6__B_DECK' , 
 'PASSAGEWAY_OUTSIDE_INFIRMARY' , 
- 
 'AIR_SHAFT' , 
 'THE_NOSTROMO__SUNSET' , 
 'PASSAGEWAY__B_LEVEL' , 
@@ -10551,7 +10365,6 @@ BRIDGE = room
 'BRIDGE' , 
 'CORRIDOR_TO_BRIDGE__A_LEVEL' , 
 'NOSTROMO__OUTER_SPACE' , 
- 
 'PLANET__SUNRISE' , 
 'OILY_CORRIDOR__C_DECK' , 
 'FOOR_LOCKER_NUMBER_6' , 
